e63594621290d6f2ff92e5ebd84d5342a48b0309
demidraksin <greatraksin@icloud.com>
2021-04-23 12:17:20 +0300

    Adding bank DB tables✅

---
+++ IV_form_databases/2304_bank/bank_script.sql
@@ -0,0 +1,35 @@
+CREATE TABLE IF NOT EXISTS account (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    customer_id INTEGER,
+    open_date DATE,
+    close_date DATE,
+    product_id INTEGER,
+    avail_balance FLOAT(10, 2),
+    pending_balance FLOAT(10, 2)
+);
+
+CREATE TABLE IF NOT EXISTS customer (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    cust_type INTEGER NOT NULL,
+    address VARCHAR(30),
+    city VARCHAR(30),
+    state VARCHAR(30),
+    postal_code VARCHAR(10)
+);
+-- таблица для физических лиц
+CREATE TABLE IF NOT EXISTS individual (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    f_name VARCHAR(30),
+    l_name VARCHAR(30),
+    passport_code VARCHAR(15),
+    phone VARCHAR(20),
+    email VARCHAR(20),
+    birth_date DATE
+);
+-- таблица для юридических лиц
+CREATE TABLE IF NOT EXISTS business (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(255) NOT NULL,
+    incorp_date DATE,
+    inp VARCHAR(50)
+);
