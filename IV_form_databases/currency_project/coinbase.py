e75cfe5df52975ad03940c597dae21cfbffd188e
demidraksin <greatraksin@icloud.com>
2021-05-14 11:56:33 +0300

    Adding script to fill the database with currencies💺

--- IV_form_databases/currency_project/coinbase.py
+++ IV_form_databases/currency_project/coinbase.py
@@ -1,8 +1,15 @@
 import requests
 import json
+from app import db
+from models import Currencies
 
 url = 'https://api.coinbase.com/v2/currencies'
 response = requests.get(url)
 cur = json.loads(response.text)
 
-print(cur)
+for i in cur['data']:
+    c = Currencies()  # cоздаю экземпляр валюты
+    c.currency_code = i['id']  # записываю код валюты
+    c.currency = i['name']  # записываю название валюты
+    db.session.add(c)  # добавляю новую валюту в бд
+    db.session.commit()  # сохраняю изменения в базе
