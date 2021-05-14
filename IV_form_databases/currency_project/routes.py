8e13ca1bb14970f498490bcc174e58a16e2a5ee0
demidraksin <greatraksin@icloud.com>
2021-05-14 12:24:58 +0300

    Adding dropdown with currencies⏰

--- IV_form_databases/currency_project/routes.py
+++ IV_form_databases/currency_project/routes.py
@@ -3,9 +3,14 @@ from flask import render_template
 # render_template - рисует html-страницы
 from app import app
 # из файла app.py импортировать переменную app
+from app import db
+from models import Currencies
 
 
 @app.route('/')  # задать ссылку на главную страницу сайта
 def main_page():  # функция отображения страницы
-    return render_template('index.html', title='Главная страница')  # что будет отображаться
+    cur_list = Currencies.query.order_by(Currencies.currency_code).all()  # выгружаю все из таблицы currencies
+    return render_template('index.html',
+                           title='Главная страница',
+                           curr=cur_list)  # что будет отображаться
 
